460deacf35babc06480cb45f0f8711f08fda2860
Kamala Kanta rout <54476924+kamala127@users.noreply.github.com>
2026-04-02 20:10:40 +0530

    Structred modified

--- pharmaflow/src/main/java/com/pharmaflow/pharmaflow/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.pharmaflow.pharmaflow.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String msg) {
-        super(msg);
-    }
-}
--- pharmaflow/src/main/java/com/pharmaflow/pharmaflow/repository/DrugRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pharmaflow.pharmaflow.repository;
-
-import com.pharmaflow.pharmaflow.entity.Drugs;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DrugRepository extends JpaRepository<Drugs, Long> {
-}
